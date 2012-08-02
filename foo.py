23def8d0e2f24b5ef920a6539ce47c0917d468df
Louis Sobel <sobel@lsmbp.(none)>
2012-08-02 22:28:25 +0000

    working, 13 lines

--- foo.py
+++ foo.py
@@ -1,11 +1,4 @@
 """
->>> is_complete([1,2,3,4,5,6,7,8,9])
-True
->>> is_complete([1,5,5,5,5,2,3,4,1])
-False
->>> is_complete([6,7,5,8,9,3,4,1,2])
-True
-
 >>> is_legal([1,0,0,0,0,0,0,0,0,0])
 True
 >>> is_legal([1,0,0,0,1,0,0,0,0,0])
@@ -16,40 +9,27 @@ True
 True
 >>> is_legal([1,0,0,0,2,0,5,5,0,0])
 False
+>>> find(board, row, 12)
+[9, 0, 0, 3, 0, 5, 0, 0, 1]
+>>> legal_moves(board, 0)
+[4, 5]
+
 """
-import pprint
-# ok so there are two interesting things rules (and three ways the rules are applied)
-# 1. <= one of each number
-# 2. every number
-
-
-
+row = lambda b : [i for i in range(b*9,(b+1)*9)]
+col = lambda b : [i for i in range(b,81,9)]
+box = lambda b : [(b//3)*27+(b%3)*3+t*9+u for t in range(3) for u in range(3)]
+is_legal = lambda nine_squares : all(len([s for s in nine_squares if s==i+1])<=1 for i in range(9))
+dex_gen = lambda section_func : (section_func(b) for b in range(9))
+find = lambda board, which, index : [[board[i] for i in nine_index] for nine_index in dex_gen(which) if index in nine_index][0]
+make_move = lambda board, index, move : [move if i == index else board[i] for i in range(81)]
+legal_moves = lambda board, index : [i for i in range(1,10) if all([is_legal(find(make_move(board, index, i), sec, index)) for sec in (row,col,box)])]
+square_moves = lambda board : (sort_tuple[1] for sort_tuple in sorted([(len(t[1]),t) for t in ( (i, legal_moves(board, i)) for i in range(81) if board[i] == 0)]))
 def solve(board):
-    is_complete = lambda nine_squares : all(i in nine_squares for i in range(9))
-    is_legal = lambda nine_squares : all(len([s for s in nine_squares if s==i+1])<=1 for i in range(9))
+    if all(board): return board #win
+    for index, move in reduce(lambda a,b : a + b, [[(index, move) for move in moves] for index, moves in square_moves(board)],[]):
+        solved = solve(make_move(board, index, move))
+        if solved is not None: return solved
 
-    row = lambda b : [i for i in range(b*9,(b+1)*9)]
-    col = lambda b : [i for i in range(b,81,9)]
-    box = lambda b : [(b//3)*27+(b%3)*3+t*9+u for t in range(3) for u in range(3)]
-
-    dex_gen = lambda section_func : (section_func(b) for b in range(9))
-    sections = lambda board, dex_set_iter : ([board[index] for index in dex_set] for dex_set in dex_set_iter)
-    find = lambda board, which, index : [nine_index for nine_index in sections(board, dex_gen(which)) if index in nine_index][0]
-
-    make_move = lambda board, index, move : [board[i] if i == index else move for i in range(81)]
-    legal_moves = lambda index : [i for i in range(1,10) if all(is_legal(find(make_move(board, index, i), sec, index) for sec in (row,col,box)))]
-    
-    empty_indicies = [i for i in range(81) if board[i] == 0]
-    if not empty_indicies: return board #win
-    
-    print type( ( (i, legal_moves(i)) for i in empty_indicies))
-    
-    square_moves = (sort_tuple[1] for sort_tuple in sorted([(len(t[1]),t) for t in ( (i, legal_moves(i)) for i in empty_indicies)]))
-    for index, moves in square_moves:
-        for move in moves:
-            solved = solve(make_move(board, index, move))
-            if solved is not None: return solved
-    return None
     
 board = [0, 0, 3,  0, 2, 0,  6, 0, 0,
          9, 0, 0,  3, 0, 5,  0, 0, 1,
@@ -82,21 +62,19 @@ for r in range(3):
 boxes = [ [(r*27+i*3)+t*9+u for t in range(3) for u in range(3)] for i in range(3) for r in range(3) ]
 '''
 
+def confirm(board):
+    pass
+
+
 if __name__ == "__main__":
-    #import doctest
-    #doctest.testmod()
-    
-    '''
-    for row in sections(board, dex_gen(row)):
-        print row
-    print '-'*27
-    for col in sections(board, dex_gen(col)):
-        print col
-    print '-'*27
-    for box in sections(board, dex_gen(box)):
-        print box
-    '''
+    import doctest
+    doctest.testmod()
+
         
 
     
     solved = solve(board)
+    
+    for row in [[solved[i] for i in d] for d in dex_gen(row)]:
+        print row
+        
