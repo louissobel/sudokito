bff87ca3fdbf4b927c1d96253c792b2fa386a573
Louis Sobel <sobel@lsmbp.local>
2012-08-05 10:55:34 +0000

    test and cleaning

--- sudoku.py
+++ sudoku.py
@@ -1,139 +1,62 @@
 """
->>> is_legal([1,0,0,0,0,0,0,0,0,0])
-True
->>> is_legal([1,0,0,0,1,0,0,0,0,0])
-False
->>> is_legal([0,0,0,0,0,0,0,0,0,0])
-True
->>> is_legal([1,0,0,0,2,0,5,0,0,0])
-True
->>> is_legal([1,0,0,0,2,0,5,5,0,0])
-False
->>> find(board, row, 12)
-[9, 0, 0, 3, 0, 5, 0, 0, 1]
->>> legal_moves(board, 0)
-[4, 5]
-
+A functional soduku solver algorithm written in 10 lines of python
+Also, a 174 line expanded version
 """
-import pprint
 
-def print_board(board):
-    for i in range(81):
-        print board[i],
-        if  i % 9 == 8:
-            print
-
-# ----------------------------------------------------------------------------------------------
-row = lambda b : [i for i in range(b*9,(b+1)*9)]
-col = lambda b : [i for i in range(b,81,9)]
-box = lambda b : [(b//3)*27+(b%3)*3+t*9+u for t in range(3) for u in range(3)]
-is_legal = lambda nine_squares : all(len([s for s in nine_squares if s==v])<=1 for v in range(1, 10))
-dex_gen = lambda section_func : (section_func(b) for b in range(9))
-find = lambda board, which, index : [[board[i] for i in nine_index] for nine_index in dex_gen(which) if index in nine_index][0]
-make_move = lambda board, index, move : [move if i == index else board[i] for i in range(81)]
-legal_moves = lambda board, index : [v for v in range(1,10) if all([is_legal(find(make_move(board, index, v), sec, index)) for sec in (row,col,box)])]
-square_moves = lambda board : [min(((i, legal_moves(board, i)) for i in range(81) if board[i] == 0),key=lambda x : len(x[1]))]
-solve_helper = lambda gen, last, tries, max : last if last else None if tries == max else solve_helper(gen, gen.next(), tries + 1, max)
-solve = lambda board : board if all(board) else [solve_helper( ( solve(make_move(board, index, move)) for index, move in [(index, move) for move in moves] ), None, 0, len(moves)) for index, moves in square_moves(board)][0]
-
-#def solve(board):
-#    if all(board): return board #win   
-#    for index, move in [[(index, move) for move in moves] for index, moves in square_moves(board)][0]:
-#        solved = solve(make_move(board, index, move))
-#        if solved is not None: return solved
-# ----------------------------------------------------------------------------------------------
-    
-board = [
-    0, 0, 3,  0, 2, 0,  6, 0, 0,
-    9, 0, 0,  3, 0, 5,  0, 0, 1,
-    0, 0, 1,  8, 0, 6,  4, 0, 0,
-             
-    0, 0, 8,  1, 0, 2,  9, 0, 0,
-    7, 0, 0,  0, 0, 0,  0, 0, 8,
-    0, 0, 6,  7, 0, 8,  2, 0, 0,
-     
-    0, 0, 2,  6, 0, 9,  5, 0, 0,
-    8, 0, 0,  2, 0, 3,  0, 0, 9,
-    0, 0, 5,  0, 1, 0,  3, 0, 0,
-]
-         
-hard_board = [
-    9, 0, 0,  0, 0, 3,  0, 0, 7,
-    8, 0, 0,  0, 0, 0,  0, 0, 1,
-    0, 0, 3,  2, 0, 0,  8, 6, 4,
-    
-    0, 6, 0,  0, 2, 7,  0, 0, 0,
-    0, 8, 1,  0, 0, 4,  0, 0, 0,
-    0, 0, 0,  0, 3, 0,  0, 0, 0,
-    
-    0, 0, 0,  0, 0, 9,  3, 5, 2,
-    0, 0, 0,  0, 0, 5,  0, 0, 0,
-    1, 0, 0,  0, 0, 0,  4, 7, 0,
-]
-
-TEST_BOARD = hard_board
-
-def is_complete(section):
+def small_solver(board):
     """
-    checks that every section is 9 long and
-    that every number appears
+    namespace the small solver    
     """
-    if not len(section) == 9:
-        return False
-    
-    for i in range(1, 10):
-        if not i in section:
-            return False
-    
-    return True
-    
-def confirm(board):
-    """
-    Confirms that a board is a winner
-    by checking every row, col, and box
-    """
-    for section_type in (row, col, box):
-        section_indicies_gen = dex_gen(section_type)
-        for section_indicies in section_indicies_gen:
-            section = [board[i] for i in section_indicies]
-            if not is_complete(section):
-                print "Invalid:", section
-                return False
-    return True
+    # The solver
+    # ----------------------------------------------------------------------------------------------
+    rows = lambda s : [i for i in range(s*9, (s+1)*9)]
+    cols = lambda s : [i for i in range(s, 81, 9)]
+    boxs = lambda s : [(s//3)*27+(s%3)*3 + (i//3)*9+(i%3) for i in range(9)]
+    is_legal = lambda ns : all(len([s for s in ns if s==v]) <= 1 for v in range(1, 10))
+    find = lambda b, s, i : [[b[j] for j in ni] for ni in (s(b) for b in range(9)) if i in ni][0]
+    make_move = lambda b, i, m : [m if j == i else b[j] for j in range(81)]
+    legal_moves = lambda b, i : [m for m in range(1, 10) if all([is_legal(find(make_move(b, i, m), s, i)) for s in (rows, cols, boxs)])]
+    square_moves = lambda b : min(((i, legal_moves(b, i)) for i in range(81) if not b[i]),key=lambda x : len(x[1]))
+    solve_helper = lambda g, l, t, m : l if l else None if t == m else solve_helper(g, g.next(), t + 1, m)
+    solve = lambda b : b if all(b) else (lambda i, ms : None if not ms else solve_helper((solve(make_move(b, i, m)) for i, m in [(i, m) for m in ms]), None, 0, len(ms)))(*square_moves(b))
+    # ----------------------------------------------------------------------------------------------
+    return solve(board)
 
-def unrolled():
+def unrolled(board):
     """
     namespace the expanded functions
+    
     """
+    # The solver
     # ----------------------------------------------------------------------------------------------
-    def row(b):
+    def rows(section_index):
         """
         Returns the nine indicies of the bth row
         """
         out = []
-        start = b * 9
-        end = (b + 1) * 9
+        start = section_index * 9
+        end = (section_index + 1) * 9
         for i in range(start, end):
             out.append(i)
         return out
         
-    def col(b):
+    def cols(section_index):
         """
         Returns the nine indicies of the bth column
         """
         out = []
-        for i in range(b, 81, 9):
+        for i in range(section_index, 81, 9):
             out.append(i)
         return out
         
-    def box(b):
+    def boxs(section_index):
         """
         Returns the nine indicies of the bth box
         """
         out = []
         for col in range(3):
             for row in range(3):
-                corner = (b // 3) * 27 + (b % 3 ) * 3
+                corner = (section_index // 3) * 27 + (section_index % 3 ) * 3
                 i = corner + row * 9 + col
                 out.append(i)
         return out
@@ -152,27 +75,14 @@ def unrolled():
                 return False
         return True
         
-    def dex_gen(section_func):
-        """
-        Given a function that takes an index and returns
-        a set of nine indicies, (like row, col, and box)
-        will return a generator that returns the value of
-        that function for each index in 0 - 9
-        """
-        index = 0
-        while True:
-            if index == 9:
-                raise StopIteration
-            yield section_func(index)
-            index += 1
         
-    def find(board, which, index):
+    def find(board, section, index):
         """
         Gets the values of the section whose generating function
         is specified by which and contains the index index
         """
-        section_indicies_gen = dex_gen(which)
-        for section_indicies in section_indicies_gen:
+        for i in range(9):
+            section_indicies = section(i)
             if index in section_indicies:
                 out = []
                 for i in section_indicies:
@@ -199,16 +109,16 @@ def unrolled():
         of all legal moves at that index
         """
         out = []
-        for v in range(1, 10):
+        for move in range(1, 10):
             legal = True
-            new_board = make_move(board, index, v)
-            for section_type in (row, col, box):
+            new_board = make_move(board, index, move)
+            for section_type in (rows, cols, boxs):
                 new_section = find(new_board, section_type, index)
                 new_section_is_legal = is_legal(new_section)
                 legal = legal * new_section_is_legal
             
             if legal:
-                out.append(v)
+                out.append(move)
         return out
     
     def square_moves(board):
@@ -216,9 +126,6 @@ def unrolled():
         Given a board, returns the empty index with
         the least amount of legal moves and the list
         of those moves as a tuple.
-        
-        The tuple is returned inside of a list
-        so it can be used in later list comprehensions
         
         The first value is the index, the second value
         is the list of legal moves for that index.
@@ -241,13 +148,37 @@ def unrolled():
                 if len(move_list) < len(best_move_list):
                     best = index_move_list
         
-        return [best]
+        return best
         
+    def solve_helper(gen, last, tries, max):
+        """
+        Used by solve to return when we find something that works
+        (so as not to try the remaining, incorrect, legal_moves)
+        
+        this replaces code in solve that may have looked like this:
+        
+        ```
+        for solution in solver_gen:
+           if solution is not None:
+               return solution
+        ```
+        
+        But doing it this way allows it to be inlined in the compressed solver
+        """
+        if last is not None:
+            return last
+        
+        if tries == max: # this avoids a StopIteration ever being raised
+            return None
+            
+        return solve_helper(gen, gen.next(), tries + 1, max)
+        
+    
     def solve(board):
         """
-        Recursively solves a sudoku by finding the set of possible moves,
-        and trying them. Returns None if it finds a square with no legal moves,
-        or if it is unable to find a winning board
+        Recursively solves a sudoku by creating a generator that will
+        repeatedly try different legal moves by recursively calling solve,
+        and passing it to solve_helper
         
         Returns the board passed in if every space is filled. That is a win
         condition, as the algorithm only makes a move if there it is legal move.
@@ -262,37 +193,149 @@ def unrolled():
             # win.
             return board
 
-        index, moves = square_moves(board)[0]
+        index, moves = square_moves(board)
         if not moves:
             return None
-                    
-        for move in moves:
-            new_board = make_move(board, index, move)
-            solved = solve(new_board)
-            if solved is not None:
-                return solved
-                
-        return None
-    
+        
+        def solver_gen():
+            for move in moves:
+                new_board = make_move(board, index, move)
+                yield solve(new_board)
+            
+        # Call solve_helper with the initial values
+        return solve_helper(solver_gen(), None, 0, len(moves))
     # ----------------------------------------------------------------------------------------------
-    solved = solve(TEST_BOARD)
+    return solve(board)
+
+# -------------------
+# Testing functions
+def print_board(board):
+    for i in range(81):
+        print board[i],
+        if  i % 9 == 8:
+            print
+            
+def is_complete(section):
+    """
+    checks that every section is 9 long and
+    that every number appears
+    """
+    if not len(section) == 9:
+        return False
+
+    for i in range(1, 10):
+        if not i in section:
+            return False
+
+    return True
+
+def confirm(board):
+    """
+    Confirms that a board is a winner
+    by checking every row, col, and box
+    """
     
-    for row in [[solved[i] for i in d] for d in dex_gen(row)]:
-        print row
+    # I have to copy these to here
+    row = lambda b : [i for i in range(b*9,(b+1)*9)]
+    col = lambda b : [i for i in range(b,81,9)]
+    box = lambda b : [(b//3)*27+(b%3)*3+t*9+u for t in range(3) for u in range(3)]
+    dex_gen = lambda section_func : (section_func(b) for b in range(9))
+    
+    for section_type in (row, col, box):
+        section_indicies_gen = dex_gen(section_type)
+        for section_indicies in section_indicies_gen:
+            section = [board[i] for i in section_indicies]
+            if not is_complete(section):
+                return False
+    return True  
+    
+def test(board_dict, solver_dict):
+    """
+    Tests the given boards with
+    the given solvers
+    """
+    import time
+    print "Testing %d sudoku solvers with %d boards" % (len(solver_dict), len(board_dict))
+    failures = False
+    for name, board in board_dict.items():
+        print '-' * 50
+        print 'Board "%s":' % name
         
+        for solver_name, solver in solver_dict.items():
+            print "Solver %s......" % solver_name,
+            start = time.time()
+            solution = solver(board)
+            end = time.time()
+            
+            if solution is None:
+                print ".. NO SOLUTION FOUND [%fs]" % (end - start)
+            elif confirm(solution):
+                print ".. OK [%fs]" % (end - start)
+            else:
+                print ".. !!!! FAILED"
+                failures = True
+                
+    if failures:
+        print "!!!! There was a failure."
         
+# ------------------
+# Testing data
+
+hn_board = [
+    0, 0, 3,  0, 2, 0,  6, 0, 0,
+    9, 0, 0,  3, 0, 5,  0, 0, 1,
+    0, 0, 1,  8, 0, 6,  4, 0, 0,
+
+    0, 0, 8,  1, 0, 2,  9, 0, 0,
+    7, 0, 0,  0, 0, 0,  0, 0, 8,
+    0, 0, 6,  7, 0, 8,  2, 0, 0,
+
+    0, 0, 2,  6, 0, 9,  5, 0, 0,
+    8, 0, 0,  2, 0, 3,  0, 0, 9,
+    0, 0, 5,  0, 1, 0,  3, 0, 0,
+]
+
+hard_board = [
+    9, 0, 0,  0, 0, 3,  0, 0, 7,
+    8, 0, 0,  0, 0, 0,  0, 0, 1,
+    0, 0, 3,  2, 0, 0,  8, 6, 4,
+
+    0, 6, 0,  0, 2, 7,  0, 0, 0,
+    0, 8, 1,  0, 0, 4,  0, 0, 0,
+    0, 0, 0,  0, 3, 0,  0, 0, 0,
+
+    0, 0, 0,  0, 0, 9,  3, 5, 2,
+    0, 0, 0,  0, 0, 5,  0, 0, 0,
+    1, 0, 0,  0, 0, 0,  4, 7, 0,
+]
+
+evil_005 = [
+    8, 0, 6,  0, 2, 0,  0, 0, 0,
+    7, 4, 0,  0, 0, 3,  0, 0, 8,
+    0, 0, 0,  0, 5, 0,  0, 3, 0,
+                              
+    5, 0, 0,  4, 0, 0,  8, 0, 0,
+    6, 0, 0,  0, 0, 0,  0, 0, 7,
+    0, 0, 7,  0, 0, 2,  0, 0, 1,
+                              
+    0, 7, 0,  0, 6, 0,  0, 0, 0,
+    4, 0, 0,  8, 0, 0,  0, 1, 6,
+    0, 0, 0,  0, 4, 0,  9, 0, 2,
+]
+
+TEST_BOARDS = {
+    'hn_board' : hn_board,
+    'hard_board' : hard_board,
+    '6.005 evil' : evil_005,
+}
+
+SOLVER_DICT = {
+    'small' : small_solver,
+    'unrolled' : unrolled,
+}
+
+
+
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-
-    '''
-    solved = solve(TEST_BOARD)
-    if not confirm(solved):
-        print "INVALID!!!!!"
-
-    for row in [[solved[i] for i in d] for d in dex_gen(row)]:
-        print row
-    '''
-
-    unrolled()
     
+    test(TEST_BOARDS, SOLVER_DICT)
